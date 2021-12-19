0ad357a03bc9190e6b135d9655f72c6b5387c530
hohooio823 <hohooio82@gmail.com>
2021-12-19 14:45:06 +0100

    Showing the map

--- dispatchmap/src/CarMap.tsx
+++ dispatchmap/src/CarMap.tsx
@@ -5,13 +5,18 @@ const containerStyle = {
   height: "600px",
 };
 
+const center = {
+  lat: 40.7831,
+  lng: -73.9712
+};
+
 const CarMap = () => {
   // At the top of this component you should subscribe to updates from the server.
   // See https://www.apollographql.com/docs/react/data/subscriptions/#executing-a-subscription
 
   return (
-    <LoadScript googleMapsApiKey="NEED_KEY_HERE">
-      <GoogleMap mapContainerStyle={containerStyle}>
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY as string}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={11.5}>
         {/* For documentation on the MapView see https://react-google-maps-api-docs.netlify.app/ */}
       </GoogleMap>
     </LoadScript>
